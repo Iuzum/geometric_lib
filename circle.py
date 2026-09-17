a4f1477550c10dec208fd74f615ef04be0d36517
Iuzum <329714072+Iuzum@users.noreply.github.com>
2026-09-18 02:33:49 +0300

    добавлены docstring-комментарии к circle.py и square.py

--- circle.py
+++ circle.py
@@ -2,9 +2,12 @@ import math
 
 
 def area(r):
+    '''Принимает радиус r (int/float), возвращает площадь круга.
+    Пример вызова: area(5) -> 78.53981633974483'''
     return math.pi * r * r
 
-
 def perimeter(r):
+    '''Принимает радиус r (int/float), возвращает длину окружности.
+    Пример вызова: perimeter(5) -> 31.41592653589793'''
     return 2 * math.pi * r
 
